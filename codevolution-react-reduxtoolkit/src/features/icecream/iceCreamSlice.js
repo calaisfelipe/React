984b4300d8bc839e3426c1fd94696f165b3fe964
Felipe Calais <felipe_calais@hotmail.com>
2023-03-14 00:06:21 -0300

    redux toolkit

--- codevolution-react-reduxtoolkit/src/features/icecream/iceCreamSlice.js
+++ codevolution-react-reduxtoolkit/src/features/icecream/iceCreamSlice.js
@@ -2,17 +2,17 @@ import {createSlice} from '@reduxjs/toolkit'
 import { ordered as cakeOrdered } from '../cake/cakeSlice'
 
 
-
 const initialState ={
     numOfIceCream: 20
 }
+
 
 const iceCreamSlice = createSlice({
 
     name: 'iceCream',
     initialState,
     reducers: {
-        ordered: (state) =>{
+        ordered: (state) => {
             state.numOfIceCream--
 
         },
