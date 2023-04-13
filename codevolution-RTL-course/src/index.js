05fdef17f9988411c9f91c3bbcaae0a3fa1dd2dd
Felipe Calais <felipe_calais@hotmail.com>
2023-04-13 13:11:59 -0300

    RTL course introduction

---
+++ codevolution-RTL-course/src/index.js
@@ -0,0 +1,11 @@
+const getName = (fName, lName) => {
+    return `${fName} ${lName}`
+}
+
+const actualName = getName('felipe2', 'calais')
+const expected = 'felipe calais'
+
+if(actualName !== expected) {
+    throw new Error( `${actualName} is not equal to ${expected}`)  
+} else{ console.log('passed')}
+
